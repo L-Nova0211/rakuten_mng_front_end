90d01522fe9956b53a7444fe32c3af8453240db7
Raccon <kanazaya74@gmail.com>
2023-08-21 15:18:53 +0900

    fix apiCall

--- src/util/apiCall.js
+++ src/util/apiCall.js
@@ -31,9 +31,6 @@ const endpoints = {
         password: password
     }),
 
-    // Get Credential
-    getCredential: () => api.get('users/get_credential/'),
-
     // Update
     update: (id, data) => api.patch(`users/${id}/`, data),
 
@@ -47,55 +44,6 @@ const endpoints = {
 
     // Get Product
     getProduct: (params) =>  api.get(`product/?${params}`),
-
-    // Sell Product
-    sellProduct: (id) => api.post(`product/${id}/sell_product/`),
-
-    // Update Product
-    updateProduct: (id, data) => api.patch(`product/${id}/`, data),
-
-    // Remove Product
-    removeProduct: (id) => api.delete(`product/${id}`),
-
-    // Bulk Sell Product
-    bulkSellProduct: (idArray) => api.post('product/bulk_sell_product/', {
-        idArray: idArray
-    }),
-
-    // Bulk Remove Product
-    bulkRemoveProduct: (idArray) => api.post('product/bulk_remove_product/', {
-        idArray: idArray
-    }),
-
-    // Download Products
-    downloadProducts: (idArray) => api.post('product/download_products/', {
-        idArray: idArray
-    }),
-
-    // Get NG
-    getNG: (params) => api.get(`ng/?${params}`),
-
-    // Register NG
-    registerNG: (type, name) => api.post('ng/register/', {
-        type: type,
-        name: name
-    }),
-
-    // Update NG
-    updateNG: (id, data) => api.patch(`ng/${id}/`, data),
-
-    // Remove NG
-    removeNG: (id) => api.delete(`ng/${id}/`),
-
-    // Bulk Active NG
-    bulkActiveNG: (idArray) => api.post('ng/bulk_active_ng/', {
-        idArray: idArray
-    }),
-    
-    // Bulk Remove NG
-    bulkRemoveNG: (idArray) => api.post('ng/bulk_remove_ng/', {
-        idArray: idArray
-    }),
 };
 
 export default endpoints;
