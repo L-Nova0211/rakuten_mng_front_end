46fea8d417412f24e2f4720f3426537bb803285d
Raccon <kanazaya74@gmail.com>
2023-10-31 08:19:15 +0900

    fix home page

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -8,6 +8,7 @@ import Button from '../component/Button';
 import Loading from '../component/Loading';
 import PaginationComponent from '../component/Pagination';
 import endpoints from '../util/apiCall';
+import sortSiteByUrl from '../util/sourceSite';
 
 
 export default function Home(){
@@ -16,26 +17,47 @@ export default function Home(){
         id: '',
         scrapeURL: '',
         editTitle: '',
-        editSellPrice: ''
+        editSellPrice: '',
+        editPoint: '',
+        editQuantity: '',
+        editShippingMethod: ''
     });
+    const shippingMethod = {
+        shipping_mail: '送料メール便',
+        shipping_60: '送料60サイズ',
+        shipping_80: '送料80サイズ',
+        shipping_100: '送料100サイズ',
+        shipping_120: '送料120サイズ'
+    };
+    const [shippingMethodDom, setShippingMethodDom] = useState([]);
     const [product, setProduct] = useState([]);
     const [warning, setWarning] = useState({
         scrapeURL: false,
         editTitle: false,
-        editSellPrice: false
+        editSellPrice: false,
+        editPoint: false,
+        editQuantity: false
     });
     const [bulkProductId, setBulkProductId] = useState([]);
     const [loading, setLoading] = useState(false);
     const [showModal, setShowModal] = useState({
-        result: false,
+        success: false,
+        error: false,
         edit: false,
-        enableAmazon: true
+        enableRakuten: true
     });
     const [message, setMessage] = useState('');
     const itemPerPage = [20, 50, 100, 200];
     const [counts, setCounts] = useState(0);
     const [current, setCurrent] = useState(1);
     const [itemPerPageValue, setItemPerPageValue] = useState(itemPerPage[0]);
+
+    useEffect(() => {
+        const options = Object.entries(shippingMethod).map(([key, value]) => (
+            <option key={key} value={key}>{value}</option>
+        ));
+        setShippingMethodDom(options);
+    }, []);
 
     useEffect(() => {
         if (!searchParams.get('page')){
@@ -103,26 +125,42 @@ export default function Home(){
         }
     }
 
-
     function handleSellProduct(id){
         setLoading(true);
-        endpoints.sellProduct(id).then((response) => {
+        endpoints.bulkInsertProduct([id]).then((response) => {
+            let data = response.data;
+            let message = '';
+            if(data['success'].length > 0) {
+                message += `${data['success'].length}件の商品が正常に登録されました。\n`
+            }
+            if(data['failed'].length > 0) {
+                message += `${data['failed'].length}件の商品登録が失敗しました。\n`
+            }
             setLoading(false);
-            setMessage(response.data);
-            setShowModal(true);
+            setMessage(message);
+            setShowModal({
+                ...showModal,
+                success: true
+            });
         }).catch((error) => {
             setLoading(false);
-            setMessage(error.response.data);
-            setShowModal(true);
+            setMessage('操作が失敗しました。もう一度お試しください。');
+            setShowModal({
+                ...showModal,
+                error: true
+            });
         });
     }
     
-    function callEditModal(id, title, sellPrice){
+    function callEditModal(id, title, sellPrice, point, quantity, editShippingMethod){
         setState({
             ...state,
             id: id,
             editTitle: title,
-            editSellPrice: sellPrice
+            editSellPrice: sellPrice,
+            editPoint: point,
+            editQuantity: quantity,
+            editShippingMethod: editShippingMethod
         });
         setShowModal({
             ...showModal, edit: true
@@ -142,19 +180,34 @@ export default function Home(){
             });
             return;
         }
+        else if(state['editPoint'] === ''){
+            setWarning({
+                ...warning, editPoint: true
+            });
+            return;
+        }
+        else if(state['editQuantity'] === ''){
+            setWarning({
+                ...warning, editQuantity: true
+            });
+            return;
+        }
         else{
             const data = {
                 title: state['editTitle'],
-                sell_price: state['editSellPrice']
-            }
+                sell_price: state['editSellPrice'],
+                point: state['editPoint'],
+                quantity: state['editQuantity'],
+                shipping_method: state['editShippingMethod']
+            };
             endpoints.updateProduct(state['id'], data).then((response) => {
-                window.location.href = '/home';
+                window.location.href = '';
             }).catch((error) => {
                 setMessage('操作が失敗しました。もう一度お試しください。');
                 setShowModal({
                     ...showModal,
                     edit: false,
-                    result: true
+                    error: true
                 });
             });
         }
@@ -162,14 +215,14 @@ export default function Home(){
 
     function handleRemoveProduct(id){
         setLoading(true);
-        endpoints.removeProduct(id).then((response) => {
-            window.location.href = '/home';
+        endpoints.bulkRemoveProduct([id]).then((response) => {
+            window.location.href = '';
         }).catch((error) => {
             setMessage('操作が失敗しました。もう一度お試しください。');
             setLoading(false);
             setShowModal({
                 ...showModal,
-                result: true
+                error: true
             });
         });
     }
@@ -216,7 +269,11 @@ export default function Home(){
             });
         }).catch((error) => {
             setLoading(false);
-            console.log(error);
+            setMessage('操作が失敗しました。もう一度お試しください。');
+            setShowModal({
+                ...showModal,
+                error: true
+            });
         });
     }
 
@@ -268,19 +325,28 @@ export default function Home(){
                 <table className='product collapse'>
                     <thead>
                         <tr>
-                            <th>
+                            <th className='checkbox'>
                                 <input type="checkbox" onChange={handleSelectAllChange} />
                             </th>
                             <th className='number'>No</th>
                             <th className='main-image'>メイン画像</th>
-                            <th className='product-url'>URL</th>
-                            <th className='product-title'>タイトル</th>
-                            {/* <th>説明</th> */}
+                            <th className='product-jan'>JAN</th>
+                            <th className='product-title'>商品名</th>
+                            <th className='product-site'>販売元</th>
+                            <th className='product-quantity'>在庫数</th>
+                            <th className='sell-price'>販売価格(円)</th>
+                            <th className='unit-price'>一つ当たり の値段 (円)</th>
+                            <th className='count-set'>入数</th>
                             <th className='buy-price'>原価(円)</th>
-                            <th className='sell-price'>販売価(円)</th>
-                            <th className='export-product'>出品</th>
-                            <th className='edit-product'>編集</th>
-                            <th className='remove-product'>削除</th>
+                            <th className='shipping-fee'>送料(円)</th>
+                            <th className='price-fee'>手数料(円)</th>
+                            <th className='point'>ポイント</th>
+                            <th className='profit'>粗利額(円)</th>
+                            <th className='profit-rate'>粗利率(%)</th>
+                            <th className='roi'>ROI(%)</th>
+                            <th className='product-edit'>編集</th>
+                            <th className='product-export'>出品</th>
+                            <th className='product-remove'>削除</th>
                         </tr>
                     </thead>
 
@@ -290,41 +356,58 @@ export default function Home(){
                                 <td>
                                     <input type="checkbox" onChange={(e) => {handleProductCheckedChange(e, item['id'])}} />
                                 </td>
-                                <td className='number'>{index + 1 + (itemPerPageValue) * (current-1)}</td>
+                                <td>{index + 1 + (itemPerPageValue) * (current-1)}</td>
                                 <td className='main-image'>
                                     {
                                         item['productphoto_set'].length > 0 &&
                                         <img src={item['productphoto_set'][0]['path']} alt="Main Image" />
                                     }
                                 </td>
-                                <td className='product-url'>
-                                    {item['source_url']}
+                                <td>
+                                    {item['jan']}
                                 </td>
-                                <td className='product-title'>
+                                <td>
                                     {item['title']}
                                 </td>
-                                {/* <td className='product-description'>
-                                    {item['description']}
-                                </td> */}
-                                <td className='buy-price'>
-                                    {item['buy_price']}
+                                <td className='product-site'>
+                                    <a href={item['source_url']} target='_blank'>{sortSiteByUrl(item['source_url'])}</a>
                                 </td>
-                                <td className='sell-price'>
+                                <td>
+                                    {item['quantity']}
+                                </td>
+                                <td>
                                     {item['sell_price']}
                                 </td>
-                                <td className='export-product'>
-                                    <Button 
-                                        text="出品"
-                                        onClick={() => handleSellProduct(item['id'])}
-                                    />
+                                <td>
+                                    {item['buy_price']}
                                 </td>
-                                <td className='edit-product'>
+                                <td>
+                                    {item['count_set']}
+                                </td>
+                                <td>
+                                    {item['buy_price']*item['count_set']}
+                                </td>
+                                <td>
+                                    {item['shipping_fee']}
+                                </td>
+                                <td>{item['rakuten_fee']}</td>
+                                <td>{item['point']}</td>
+                                <td>{item['profit']}</td>
+                                <td>{(item['profit']*100/item['sell_price']).toFixed(2)}</td>
+                                <td>{(item['profit']*100/(item['buy_price']*item['count_set']*1.1)).toFixed(2)}</td>
+                                <td>
                                     <Button 
                                         text='編集'
-                                        onClick={() => {callEditModal(item['id'], item['title'], item['sell_price'])}}
+                                        onClick={() => {callEditModal(item['id'], item['title'], item['sell_price'], item['point'], item['quantity'], item['shipping_method'])}}
                                     />
                                 </td>
-                                <td className='remove-product'>
+                                <td>
+                                    <Button 
+                                        text='出品'
+                                        onClick={() => {handleSellProduct(item['id'])}}
+                                    />
+                                </td>
+                                <td>
                                     <Button 
                                         text='削除'
                                         onClick={() => {handleRemoveProduct(item['id'])}}
@@ -342,13 +425,13 @@ export default function Home(){
             />
 
             {/* Edit Modal */}
-            <Modal visible={showModal['edit']} width="600" height="300" onClickAway={() => {setShowModal({...showModal, edit: false})}}>
+            <Modal visible={showModal['edit']} width="600" height="500" onClickAway={() => {setShowModal({...showModal, edit: false})}}>
                 <div className='modal edit'>
                     <Input 
                         className='row'
                         type='text'
                         name='editTitle'
-                        label='タイトル'
+                        label='商品名'
                         value={state['editTitle']}
                         warning={warning['editTitle']}
                         onChange={handleChange}
@@ -357,11 +440,35 @@ export default function Home(){
                         className='row'
                         type='number'
                         name='editSellPrice'
-                        label='販売価'
+                        label='販売価格'
                         value={state['editSellPrice']}
                         warning={warning['editSellPrice']}
                         onChange={handleChange}
                     />
+                    <Input 
+                        className='row'
+                        type='number'
+                        name='editPoint'
+                        label='ポイント'
+                        value={state['editPoint']}
+                        warning={warning['editPoint']}
+                        onChange={handleChange}
+                    />
+                    <Input 
+                        className='row'
+                        type='number'
+                        name='editQuantity'
+                        label='在庫数'
+                        value={state['editQuantity']}
+                        warning={warning['editQuantity']}
+                        onChange={handleChange}
+                    />
+                    <div className="row">
+                        <div className="label">発送方法</div>
+                        <select name="editShippingMethod" value={state['editShippingMethod']} onChange={handleChange}>
+                            {shippingMethodDom}
+                        </select>
+                    </div>
                     <Button 
                         className='row'
                         btnClassName='btn btn-green'
@@ -372,8 +479,8 @@ export default function Home(){
                 </div>            
             </Modal>                    
 
-            {/* Result Modal */}
-            <Modal visible={showModal['result']} width="400" height="200" onClickAway={() => {setShowModal({...showModal, result: false})}}>
+            {/* Success Modal */}
+            <Modal visible={showModal['success']} width="400" height="200" onClickAway={() => {setShowModal({...showModal, success: false})}}>
                 <div className='modal result'>
                     <div className='message row'>
                         {message}
@@ -382,13 +489,28 @@ export default function Home(){
                         className='row'
                         btnClassName='btn btn-green'
                         text='確認'
-                        onClick={() => {window.location.href = '/home'}}
+                        onClick={() => {window.location.href = '/productManager'}}
+                    />
+                </div>
+            </Modal>
+
+            {/* Error Modal */}
+            <Modal visible={showModal['error']} width="400" height="200" onClickAway={() => {setShowModal({...showModal, error: false})}}>
+                <div className='modal result'>
+                    <div className='message row'>
+                        {message}
+                    </div>
+                    <Button 
+                        className='row'
+                        btnClassName='btn btn-green'
+                        text='確認'
+                        onClick={() => {window.location.href = ''}}
                     />
                 </div>
             </Modal>
 
             {/* Enable Amazon */}
-            <Modal visible={!showModal['enableAmazon']} width="400" height="200">
+            <Modal visible={!showModal['enableRakuten']} width="400" height="200">
                 <div className='modal result'>
                     <div className='message row'>
                         設定ページで「AmazonCredential」情報を入力します。
